c746893c4b5da24d8290ce87aa6db1ead8be159f
Meenal_Solanki <66253852+Meenalsolanki220397@users.noreply.github.com>
2024-10-16 07:05:52 +0000

    mmainting state via parent and understanding immutability

--- src/App.js
+++ src/App.js
@@ -19,19 +19,65 @@ export default function Board() {
   return (
     <>
       <div className="board-row">
-        <Square value={squares[0]} onSquareClick={handleClick(0)} />
-        <Square value={squares[1]} onSquareClick={handleClick(1)} />
-        <Square value={squares[2]} onSquareClick={handleClick(2)} />
+        {/* The handleClick(0) call will be a part of rendering the board component. Because handleClick(0) alters the state of the board component by calling setSquares, your entire board component will be re-rendered again. But this runs handleClick(0) again, leading to an infinite loop:---!> */}
+        <Square
+          value={squares[0]}
+          onSquareClick={() => {
+            handleClick(0);
+          }}
+        />
+        <Square
+          value={squares[1]}
+          onSquareClick={() => {
+            handleClick(1);
+          }}
+        />
+        <Square
+          value={squares[2]}
+          onSquareClick={() => {
+            handleClick(2);
+          }}
+        />
       </div>
       <div className="board-row">
-        <Square value={squares[3]} onSquareClick={handleClick(3)} />
-        <Square value={squares[4]} onSquareClick={handleClick(4)} />
-        <Square value={squares[5]} onSquareClick={handleClick(5)} />
+        <Square
+          value={squares[3]}
+          onSquareClick={() => {
+            handleClick(3);
+          }}
+        />
+        <Square
+          value={squares[4]}
+          onSquareClick={() => {
+            handleClick(4);
+          }}
+        />
+        <Square
+          value={squares[5]}
+          onSquareClick={() => {
+            handleClick(5);
+          }}
+        />
       </div>
       <div className="board-row">
-        <Square value={squares[6]} onSquareClick={handleClick(6)} />
-        <Square value={squares[7]} onSquareClick={handleClick(7)} />
-        <Square value={squares[8]} onSquareClick={handleClick(8)} />
+        <Square
+          value={squares[6]}
+          onSquareClick={() => {
+            handleClick(6);
+          }}
+        />
+        <Square
+          value={squares[7]}
+          onSquareClick={() => {
+            handleClick(7);
+          }}
+        />
+        <Square
+          value={squares[8]}
+          onSquareClick={() => {
+            handleClick(8);
+          }}
+        />
       </div>
     </>
   );
