b6c5be11c09a72141f626cfccd7487a33f66b3fc
Meenal_Solanki <66253852+Meenalsolanki220397@users.noreply.github.com>
2024-10-17 05:31:49 +0000

    Decaling winner for Tic Tac Toe game and added logic

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ export default function Board() {
 
   const handleClick = (i) => {
     // adding validation check if tile is filled then user cant edit it, only empty tile can be fit with X or O
-    if (squares[i]) {
+    if (squares[i] || calculateWinner(squares)) {
       return;
     }
     const nextSquares = squares.slice();
@@ -27,8 +27,43 @@ export default function Board() {
     setXIsNext(!xIsNext);
     setSquares(nextSquares);
   };
+
+  // defining all formats for winning
+  const calculateWinner = (squares) => {
+    const winnerFormates = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+    for (let i = 0; i < winnerFormates.length; i++) {
+      const [a, b, c] = winnerFormates[i];
+      if (
+        squares[a] &&
+        squares[a] === squares[b] &&
+        squares[a] === squares[c]
+      ) {
+        return squares[a];
+      }
+    }
+    return null;
+  };
+
+  const winner = calculateWinner(squares);
+  let status;
+  if (winner) {
+    status = "Winner: " + winner;
+  } else {
+    status = "Next player: " + (xIsNext ? "X" : "O");
+  }
+
   return (
     <>
+      <div className="status">{status}</div>
       <div className="board-row">
         {/* The handleClick(0) call will be a part of rendering the board component. Because handleClick(0) alters the state of the board component by calling setSquares, your entire board component will be re-rendered again. But this runs handleClick(0) again, leading to an infinite loop:---!> */}
         <Square
